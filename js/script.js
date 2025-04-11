290e5626d1e9e0189091e9301f2b70c7a25c4deb
Luiz Gunigit <seuemail@exemplo.com>
2025-04-11 15:03:22 -0300

    Atualização do portfólio com imagens, estilo e JS

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-// Turns the menu hamburguer into a X //
-
+// Seleciona o elemento do menu hambúrguer
 const menuHamburguer = document.querySelector('.menu-hamburguer')
+
+// Adiciona um evento de clique ao menu hambúrguer
 menuHamburguer.addEventListener('click', () => {
-    toggleMenu();
+    toggleMenu(); // Chama a função que alterna a exibição do menu
 });
 
+// Função que alterna o estado do menu (mostrar ou esconder)
 function toggleMenu() {
-    const nav = document.querySelector('.nav-responsive')
+    const nav = document.querySelector('.nav-responsive') // Seleciona o menu de navegação responsivo
+
+    // Alterna a classe 'change' no menu hambúrguer (ativa/desativa a animação para "X")
     menuHamburguer.classList.toggle('change');
 
+    // Se a classe 'change' estiver presente, exibe o menu
     if(menuHamburguer.classList.contains('change')) {
         nav.style.display = 'block';
-
     } else {
+        // Caso contrário, esconde o menu
         nav.style.display = 'none'; 
     }
 }
